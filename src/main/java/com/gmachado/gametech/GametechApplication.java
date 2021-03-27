98298985163ce0d306ac3eba5be872e5f617f33d
Gabriel Machado <g_machado1@hotmail.com>
2021-03-27 14:57:20 -0300

    Initialized project with springboot

---
+++ src/main/java/com/gmachado/gametech/GametechApplication.java
@@ -0,0 +1,13 @@
+package com.gmachado.gametech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmachado/gametech/GametechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmachado.gametech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
